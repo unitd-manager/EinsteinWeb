d286c5c6c1ae7f43e020aaada768760182ecad84
rafiunited <113578730+rafiunited@users.noreply.github.com>
2025-01-27 13:55:38 +0530

    rafi commit

--- src/constants/api.js
+++ src/constants/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
 // baseURL: 'http://43.228.126.245:4023',
-baseURL: 'http://192.64.114.83:2061',
+baseURL: 'https://ecas.unitdtechnologies.com:2062',
 // baseURL: 'http://localhost:3001'
 });
 
